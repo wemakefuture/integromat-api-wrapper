5385818492a3952ce9d8338ca6b48c950451900d
probook <d.p.arnold@gmx.de>
2021-05-24 20:34:23 +0200

    minor changes

--- examples/simpleExample.js
+++ examples/simpleExample.js
@@ -2,8 +2,8 @@ const Integromat = require("../integromat-node.js");
 
 (async function () {
 
-    var fullAccess = new Integromat("ecb80230-6b5a-4795-8d04-bd4e46cf1941")
-    fullAccess.templates.list({}, {})
+    var integromat = new Integromat("ecb80230-6b5a-4795-8d04-bd4e46cf1941")
+    integromat.templates.list({}, {})
         .then(res => res.json())
         .then(json => console.log(json));
 
---
+++ experiments/plutio-modules.js
@@ -0,0 +1,39 @@
+const Integromat = require("../integromat-node.js");
+
+(async function () {
+
+    var integromat = new Integromat("ecb80230-6b5a-4795-8d04-bd4e46cf1941")
+
+    let scenarioID = 132; 
+    let scenario = await integromat.scenarios.detail(
+        {
+            "scenarioId": scenarioID
+        }, {})
+        .then(res => res.json())
+        .then(json => json.scenario)
+    console.log(JSON.stringify(scenario, null, 4))
+
+    blueprintVersion = await integromat.scenarios.blueprints.list(
+        {
+            "scenarioId": scenarioID,
+            "pg[sortBy]": "version",
+            "pg[sortDir]": "desc",
+            "pg[limit]": 1
+
+        }, {})
+        .then(res => res.json())
+        .then(json => json.scenariosBlueprints[0].version)
+
+
+    // ###### Read out blueprint
+    let blueprint = await integromat.scenarios.blueprint(
+        {
+            "scenarioId": scenarioID,
+            "blueprintId": blueprintVersion
+        }, {})
+        .then(res => res.json())
+        .then(json => json.response.blueprint)
+        .then(result => console.log(JSON.stringify(result, null, 4)));
+
+
+})();
