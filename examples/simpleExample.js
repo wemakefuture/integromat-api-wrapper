d0bd082723c6817a6c0892d7fd2167de5ca58082
wemakefuture <dev@wemakefuture.com>
2021-06-21 18:46:24 +0200

    Update simpleExample.js

--- examples/simpleExample.js
+++ examples/simpleExample.js
@@ -2,7 +2,7 @@ const Integromat = require("../integromat-node.js");
 
 (async function () {
 
-    var integromat = new Integromat("ecb80230-6b5a-4795-8d04-bd4e46cf1941")
+    var integromat = new Integromat("ecb80230-6b5a-4795-8d04-*****")
     integromat.templates.list({}, {})
         .then(res => res.json())
         .then(json => console.log(json));
